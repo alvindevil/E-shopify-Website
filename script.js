cf780ecbd147a162bfeea30e1929dff06fcb7783
Shivam Yadav <69305996+alvindevil@users.noreply.github.com>
2023-06-09 12:50:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function leftscroll()
+{
+    const left = document.getElementById("scroll_box");
+    left.scrollBy(-200,0);
+}
+function rightscroll()
+{
+    const right = document.getElementById("scroll_box");
+    right.scrollBy(200,0);
+}
